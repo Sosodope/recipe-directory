b7845d7d7a684da6cf02296e3ddb6f793d560143
Sosodope <sonadolkar93@hotmail.com>
2018-08-20 08:15:13 +0700

    Add form component and fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./components/Form";
+
+const API_KEY = "c45ac00e75670f133f58855a5e78629b";
 
 class App extends Component {
+  state = {
+    recipes: []
+  };
+  getRecipes = async e => {
+    e.preventDefault();
+    const searchTerm = e.target.elements.searchTerm.value;
+    const api_call = await fetch(
+      `https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${searchTerm}&count=10`
+    );
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes });
+  };
   render() {
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Recipe Search</h1>
+          <Form recipe={this.state.recipes} getRecipes={this.getRecipes} />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <ul>
+            {this.state.recipes.map(recipe => {
+              return <li key={recipe.recipe_id}>{recipe.title}</li>;
+            })}
+          </ul>
+        </div>
       </div>
     );
   }
